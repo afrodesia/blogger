c815a6df4f7b4572baa83f8a0eb19de5b89d9a5d
Ken Torry <kentorry@afrodesiamedia.com>
2014-04-26 18:03:40 -0500

    Continue Post Controller

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -6,6 +6,14 @@ class PostController extends BaseController{
 	{
 		$post = Post::orderBy('id', 'desc')->paginate(10);
 		$this->layout->title = 'Post listings';
+		$this->layout->main = View::make('dash')->nest('content', 'post.list', compact('post'));
+	}
+
+	public funtion showPost()
+	{
+		$comments = $post->comments()->where('approved', '=', 1)->get();
+		$this->layout->title = $post->title;
+		$this->layout->main = View::make('home')->nest('content', 'post.single', compact('post', 'comments'));
 	}
 
 }
